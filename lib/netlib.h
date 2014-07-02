713d01928ac8aa27dedb075b7d4fcc1c8f9551e5
JessicaDong <d.ruyue@gmail.com>
2014-07-02 21:46:10 +0800

    The general library

---
+++ lib/netlib.h
@@ -0,0 +1,9 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
